43abc15ced6125096f6c4f2381bb524bfaa9c93d
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-08-08 22:15:25 -0700

    done

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:xo_game/constant.dart';
+import 'package:xo_game/data/board_data.dart';
 import 'package:xo_game/views/game_view.dart';
 import 'package:xo_game/widgets/custom_button.dart';
 
@@ -16,7 +17,7 @@ class HomeView extends StatelessWidget {
           children: [
             Image.asset(
               'assets/images/xo-logo.png',
-              height: 111,
+              height: 200,
             ),
             const SizedBox(height: 30),
             CustomButton(
@@ -24,7 +25,26 @@ class HomeView extends StatelessWidget {
                 Navigator.push(context,
                     MaterialPageRoute(builder: (context) => const GameView()));
               },
-              text: 'Start Game',
+              text:(numOfWinsForO > 0 || numOfWinsForX > 0)? 'Complete Game': 'Start Game',
+              color: Colors.orange.shade900,
+            ),
+            const SizedBox(height: 40),
+            
+            CustomButton(
+              onPressed: () {
+                counter = 0;
+                boardCells = ['', '', '', '', '', '', '', '', ''];
+                gameOver = false;
+                currentPlayer = 'o';
+                xIsPlay = false;
+                turnXOBoard = false;
+                numOfWinsForO = 0;
+                numOfWinsForX = 0;
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => const GameView()));
+              },
+              text: 'New Game',
+              color: Colors.purple.shade900,
             ),
           ],
         ),
